59d0a9c6f0f8b65e6d15a113036f8c04319d9957
kespa <kespa71for@gmail.com>
2024-10-27 17:32:37 +0100

    add Context:

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,14 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+import { CartContext } from "../store/ShopContext.jsx";
+
+export default function Cart() {
+  const {items, updateItemsToCart} = useContext(CartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
+
   const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
 
   return (
@@ -20,11 +26,11 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemsToCart(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemsToCart(item.id, 1)}>
                     +
                   </button>
                 </div>
